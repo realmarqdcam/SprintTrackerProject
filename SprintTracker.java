f1aa3873b6d030bdad76600bfcccf10c466e474b
Marques Cameron <marqdcam@outlook.com>
2026-04-02 15:46:47 -0500

    Test database implemented, first entry, & updated logic to insert data.

--- SprintTracker.java
+++ SprintTracker.java
@@ -1,27 +1,26 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class SprintTracker{
     public static void main (String [] args) {
-        System.out.println("Sprint Tracker Project loading...");
-        String url = "jdbc:sqlite:C:/Users/marqd/NewProjectJST/Sessions.db";
+        String url = "jdbc:sqlite:C:/Users/marqd/NewProjectJST/test.db";
 
-        System.out.println(new java.io.File("Sessions.db").getAbsolutePath());
+        System.out.println(new java.io.File("test.db").getAbsolutePath());
 
         try (Connection conn = DriverManager.getConnection(url)) {
+
+            
+            String sql = "INSERT INTO test (ID, Date, Event, Time_Seconds, Note) VALUES (0, '1/16/2026', 400, '49.58', 'N/A')";
+            Statement stmt = conn.createStatement();
+
             if (conn != null) {
                 System.out.println("Connected to the database successfully!");
             }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-        {
-            String sql = "INSERT INTO Sessions (ID, Date, Event, Time, Note) VALUES (0, '1/16/2026', 400, '49.58', 'N/A')";
-                conn.createStatement().execute(sql);
-                    int x = stmt.executeUpdate(sql);
+            
+            int x = stmt.executeUpdate(sql);
                         if ( x > 0) {
                             System.out.println("Recorded successfully.");
                         }
@@ -29,6 +28,10 @@ public class SprintTracker{
                         System.out.println("Insert failed.");
                             conn.close();
                         }
+                        
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
+
     }
 }
