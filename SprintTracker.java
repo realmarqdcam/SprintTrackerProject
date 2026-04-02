ba9b43ff2cf23b7b885b77ce2e9cded14b983b56
Marques Cameron <marqdcam@outlook.com>
2026-04-02 09:49:28 -0500

    Settings and .jar file.

--- SprintTracker.java
+++ SprintTracker.java
@@ -1,3 +1,6 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class SprintTracker{
     public static void main (String [] args) {
         System.out.println("Sprint Tracker Project loading...");
