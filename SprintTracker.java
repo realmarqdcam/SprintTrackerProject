d173570d13489173419ee3ad7f801d87e6d42cd8
Marques Cameron <marqdcam@outlook.com>
2026-04-02 13:59:51 -0500

    First database run and update on SprintTracker.java logic

--- SprintTracker.java
+++ SprintTracker.java
@@ -1,8 +1,34 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class SprintTracker{
     public static void main (String [] args) {
         System.out.println("Sprint Tracker Project loading...");
+        String url = "jdbc:sqlite:C:/Users/marqd/NewProjectJST/Sessions.db";
+
+        System.out.println(new java.io.File("Sessions.db").getAbsolutePath());
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                System.out.println("Connected to the database successfully!");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        {
+            String sql = "INSERT INTO Sessions (ID, Date, Event, Time, Note) VALUES (0, '1/16/2026', 400, '49.58', 'N/A')";
+                conn.createStatement().execute(sql);
+                    int x = stmt.executeUpdate(sql);
+                        if ( x > 0) {
+                            System.out.println("Recorded successfully.");
+                        }
+                        else {
+                        System.out.println("Insert failed.");
+                            conn.close();
+                        }
+        }
     }
 }
